635649c015a6d1b888d9c07088fe607fb3cec88a
Quintonyatich7533 <amusavikhamasi@gmail.com>
2023-07-17 10:19:03 +0300

    django project

--- Sakaidjango/code.py
+++ Sakaidjango/code.py
@@ -1,6 +1,11 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
+
 git remote add origin https://github.com/Namusavi/Djangolearn.git
-git push -u origin main
+
+
+
+1.git init
+2.git config user.email "amusavikhamasi@gmail.com"
+3.git add .
+4.git commit -m "first commit"
+5. replace with yours - git remote add origin https://github.com/devtedke/devtedke.git
+6.git push -u origin master
