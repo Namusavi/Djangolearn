56b1667e2c075b9d971e1dbdf00a3a8e54dce180
Quintonyatich7533 <amusavikhamasi@gmail.com>
2023-07-24 08:46:24 +0300

    django project

--- Sakaidjango/views.py
+++ Sakaidjango/views.py
@@ -13,3 +13,9 @@ def projectspage(request):
 
 def blogpage(request):
     return render(request,"blog.html")
+
+def indexpage(request):
+    return render(request,"index.html")
+
+def signuppage(request):
+    return render(request,"signup.html")
